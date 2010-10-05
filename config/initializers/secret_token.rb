89ae285c5ea9742357b9b6ed97e9eb204f5651a7
murata <eringimazui@gmail.om>
2010-10-05 11:04:46 +0900

    rails new kaisen_murata

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaisenMurata::Application.config.secret_token = '059b9034f0d2078c75a3e199a95c7c18684ec1f717d2893fbd502c420c32b81caa82ae5fa1cf0f99e6b049ea6e84e49d0175a0c1f99fe435666e84746e6e36ea'
