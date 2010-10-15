10027375c238689ea09a192f9d15a555ffc03774
murata <eringimazui@gmail.om>
2010-10-15 17:07:50 +0900

    add recommended to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101015055055) do
+ActiveRecord::Schema.define(:version => 20101015075249) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20101015055055) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "price",       :default => 0
+    t.boolean  "recommended"
   end
 
   create_table "shops", :force => true do |t|
