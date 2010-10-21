d2c88b8e5b6708763c1fc21acd61c5ae9583a81d
murata <eringimazui@gmail.om>
2010-10-21 17:07:56 +0900

    add paginate for products to /store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,6 @@
 class StoreController < ApplicationController
   def index
-	@products = Product.for_sale
+	@products = Product.for_sale.for_sale.paginate :page => params[:page], :per_page => 5
   end
 
 end
