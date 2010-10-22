98cc7a2149dbdb40e61405721d8737b4c3b151d1
murata <eringimazui@gmail.om>
2010-10-22 14:05:22 +0900

    5824120

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,14 @@
 class Cart
-  def initialize
-    @item_ids = []
-  end
-
   def add_product(product)
     @item_ids << product.id
   end
 
   def items
-    Product.find(@item_ids)
+   #Product.find(@item_ids)
+   item_ids.map{|id| Product.find(id)}
+  end
+
+  def item_ids
+    @item_ids ||= []
   end
 end
