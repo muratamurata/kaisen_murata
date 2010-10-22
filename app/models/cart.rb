c287ae62732e022ac9de4b5ff9bd118f3140062e
murata <eringimazui@gmail.om>
2010-10-22 14:57:15 +0900

    add quantity to cart item

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,14 +1,16 @@
 class Cart
   def add_product(product)
-    @item_ids << product.id
+    quantities_by_item_id[product.id] += 1
   end
 
-  def items
-   #Product.find(@item_ids)
-   item_ids.map{|id| Product.find(id)}
-  end
+  def items_with_quantity
+    quantities_by_item_id.map do |product_id, quantity|
+      [Product.find(product_id), quantity]
+    end
+  end 
 
-  def item_ids
-    @item_ids ||= []
+  private
+  def quantities_by_item_id
+    @quantites_by_item_id ||= Hash.new(0)
   end
 end
