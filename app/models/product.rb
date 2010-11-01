d1957cd53bd661e21b2c6df2a47e7e15ed3607b5
murata <eringimazui@gmail.om>
2010-11-01 18:45:22 +0900

    shop_id add to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :line_items
 
-  validates_presence_of :name, :description, :image_url
-  validates_numericality_of :price, :greater_than => 0, :only_integer => true
+  validates_presence_of :name, :description, :image_url, :shop_id
+  validates_numericality_of :price, :shop_id, :greater_than => 0, :only_integer => true
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,
                       :message => "はGIF,JPG,PNG画像のURLでなければなりません"
   validates_uniqueness_of :name
